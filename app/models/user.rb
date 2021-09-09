f34b90c6dd6d27d410877599482f875af0ce1bbe
sef <sefmangampo@gmail.com>
2021-09-09 22:51:24 +0800

    fixed user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :rememberable, :validatable
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable :registerable
+  devise :database_authenticatable, :rememberable, :validatable, :registerable
 
   validates :username, presence: true, uniqueness: { case_sensitive: false }
 
